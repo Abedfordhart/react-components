329f6677835fd3dd96712b148f286ad7ee8f1516
Andrew Bedford-Hart <abedfordhart@gmail.com>
2017-04-12 16:41:12 -0700

    Complete React Components

--- app.js
+++ app.js
@@ -1,1 +1,50 @@
-// TODO
+//TODO
+class GroceryListItem extends React.Component {
+ constructor(props){
+  super(props);
+  this.state = {
+    done: false
+  };
+ }
+
+ onListItemClick(){
+  this.setState({
+    done: !this.state.done
+  })
+ }
+
+ render(){
+   
+   var style = {
+    textDecoration: this.state.done ? 'line-through' : 'none'
+   };
+
+  return (
+     <li style={style} onClick={this.onListItemClick.bind(this)}>{this.props.todo}</li>
+  );
+ }
+}
+
+var GroceryList = (props) => (
+  <ul>
+    {props.todos.map(todo =>
+      <GroceryListItem todo={todo}/>
+      )}
+  </ul>
+);
+
+var App = () => (
+  <div>
+    <h2> Grocery List </h2>
+      <GroceryList todos = {['milk', 'cookies', 'wine']}/>
+  </div>
+);
+
+ReactDOM.render(<App/>, document.getElementById('app'));
+
+
+
+ // <div>
+ //     <h2>Grocery List</h2>
+ //     <GroceryListItem todos = {['milk', 'cookies', 'wine']}/>
+ //    </div>
